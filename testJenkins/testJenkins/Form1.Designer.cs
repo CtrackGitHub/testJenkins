8b7602a5bfa7af878b75cd6a9be3d8973804c1c8
wesssel.vdwalt <wesse.vdwalt@inseego.com>
2018-06-25 12:13:50 +0200

    added Microsoft.Net.Compilers nuget

--- testJenkins/testJenkins/Form1.Designer.cs
+++ testJenkins/testJenkins/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 69);
+            this.button1.Location = new System.Drawing.Point(81, 151);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -54,7 +54,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(81, 107);
+            this.label1.Location = new System.Drawing.Point(78, 79);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 2;
